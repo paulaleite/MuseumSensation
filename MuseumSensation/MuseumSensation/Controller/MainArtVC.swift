92492beeb47c54176e3e457573ba634e158884f0
Fabrício <guilhermo.fabricio@gmail.com>
2019-08-26 17:44:57 -0300

    tela quase pronta, falta o degrade

--- MuseumSensation/MuseumSensation/Controller/MainArtVC.swift
+++ MuseumSensation/MuseumSensation/Controller/MainArtVC.swift
@@ -9,22 +9,47 @@
 import UIKit
 
 class MainArtVC: UIViewController {
-
+    @IBOutlet weak var mainArt: UIImageView!
+    @IBOutlet weak var playButton: UIImageView!
+    @IBOutlet weak var secondArt: UIView!
+    @IBOutlet weak var microphone: UIImageView!
+    @IBOutlet weak var artNameLabel: UILabel!
+    @IBOutlet weak var gradientLayer: UIView!
+    
+    let iphoneNotch: CGFloat = 30
+    let distanceToBorders: CGFloat = 28
+    let roundedBorder: CGFloat = 6
+    let zeroPoint: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // image size equal main view size
+        mainArt.frame = view.frame
+        
+        // set play button position
+        playButton.center.x = view.center.x
+        playButton.center.y = view.frame.height - playButton.frame.height/2 - distanceToBorders
+        
+        // frame edited
+        secondArt.layer.cornerRadius = roundedBorder
+        secondArt.center.x = zeroPoint + secondArt.frame.width/2 + distanceToBorders
+        
+        // microphone
+        microphone.center.x = view.frame.width -  microphone.frame.width/2 - distanceToBorders
+        microphone.center.y = playButton.center.y
+        
+        // art name
+        artNameLabel.center.x = view.center.x
+        artNameLabel.center.y =  artNameLabel.frame.height/2 + distanceToBorders + iphoneNotch
+        
+        let gradient = CAGradientLayer()
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 0.6)
+        let whiteColor = UIColor.white
+        gradient.colors = [whiteColor.withAlphaComponent(0.0).cgColor, whiteColor.withAlphaComponent(1.0).cgColor, whiteColor.withAlphaComponent(1.0).cgColor]
+        gradient.locations = [NSNumber(value: 0.0),NSNumber(value: 0.2),NSNumber(value: 1.0)]
+        gradient.frame = gradientLayer.bounds
+        gradientLayer.layer.mask = gradient
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
