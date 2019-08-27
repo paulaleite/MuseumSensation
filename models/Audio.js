6861a015f7625dc01007c12e5a3f9344ccf6cf55
Paula Leite <paula.t.leite@icloud.com>
2019-08-27 17:33:54 -0300

    Adaptions to the Audio and Obra entities and to the server.js, adding the get, put, post and delete

--- models/Audio.js
+++ models/Audio.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Audio = new Schema({
-  dados: {
+  nome: {
     type: String
   },
   estado: {
     type: Boolean
-  },
-  nomeObra: {
-    type: String
   },
   curador: {
       type: Boolean
